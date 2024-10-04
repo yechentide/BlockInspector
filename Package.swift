edadd5ae32907c0c1b2a380b36a65727200ed1c3
yechentide <yechentide@gmail.com>
2024-10-04 23:05:45 +0900

    chore: add CoreBedrock as a package dependency for data analysis

--- Package.swift
+++ Package.swift
@@ -7,14 +7,14 @@ let package = Package(
     name: "BlockInspector",
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
+        .package(url: "https://github.com/yechentide/CoreBedrock.git", revision: "9b5cceb8a9a623fcb940ca3f3cf994b5eebdb589"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
             name: "BlockInspector",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "CoreBedrock", package: "CoreBedrock")
             ]
         ),
     ]
